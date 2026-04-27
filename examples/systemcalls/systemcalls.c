63167e81bacea06a816da6614cc65632fcbde827
rkhoury <rkhoury.eng@outlook.com>
2026-04-27 23:19:45 +0300

    assignment-3-part-1 - attempt 2

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -64,7 +64,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
     /*
      * TODO:
      *   Execute a system command by calling fork, execv(),
@@ -132,7 +132,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    // command[count] = command[count];
 
     /*
      * TODO
